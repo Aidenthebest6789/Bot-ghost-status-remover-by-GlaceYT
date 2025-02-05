8bc30229210de394effd6f17f497504dedf2c623
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-02-04 19:43:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["✝ Watching Faithfull Fellowship ✝", "✝ Moderating Faithfull Fellowship ✝"];
+const statusMessages = [" Watching The server ", " Moderating The server "];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
